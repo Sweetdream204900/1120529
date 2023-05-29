1612086197b889efdb2ff84ffb19d85ef79cb294
Sweetdream204900 <134909717+Sweetdream204900@users.noreply.github.com>
2023-05-29 15:27:39 +0800

    Update index.py

--- index.py
+++ index.py
@@ -9,6 +9,25 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+import requests
+
+def checkword(w):
+    url = 'https://www.moedict.tw/uni/' + w
+    r = requests.get(url)
+    datas = r.json()
+    msg = '國字：' + datas['title'] + '\n'
+    msg += '部首：' + datas['radical'] + '\n'
+    msg += '筆劃：' + str(datas['stroke_count']) + '\n\n'
+    for i in range(len(datas['heteronyms'])):
+        msg += '注音：' + datas['heteronyms'][i]['bopomofo'] + '\n'
+        msg += '拼音：' + datas['heteronyms'][i]['pinyin'] + '\n'    
+        for j in range(len(datas['heteronyms'][i]['definitions'])):
+            if 'type' in datas['heteronyms'][i]['definitions'][j]:
+                msg += '[{}] {}\n'.format(
+                    datas['heteronyms'][i]['definitions'][j]['type'],
+                    datas['heteronyms'][i]['definitions'][j]['def'])
+        msg += '\n'
+    return msg
 
 app = Flask(__name__)
 
